ba97b9979a3ea7f8183dbb51bd5ad85c7e38cbe2
YueSara <51194867+YueSara@users.noreply.github.com>
2019-05-30 12:03:18 +0200

    Delete CVLoader.java

--- src/projekt/simsearch/CVLoader.java
+++
@@ -1,9 +0,0 @@
-package projekt.simsearch;
-
-import java.io.File;
-
-public class CVLoader {
-	public static void load() {
-		System.load(new File("libs/libopencv_java248.dylib").getAbsolutePath());
-	}
-}
